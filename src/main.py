6806254986941e9806b8605e6687d1036c1507d6
John Chen <jmc_tt@live.com>
2025-10-08 11:11:27 -0400

    Go back to v3

--- src/main.py
+++ src/main.py
@@ -1,22 +1,12 @@
 from nicegui import ui
-from tabs.home_tab import home_tab
-from tabs.settings_tab import settings_tab
 
-def main():
-    # Define the tab headers
-    with ui.tabs() as tabs:
-        tab1 = ui.tab('Home')
-        tab2 = ui.tab('Settings')
+with ui.tabs().classes('w-full') as tabs:
+    one = ui.tab('One')
+    two = ui.tab('Two')
+with ui.tab_panels(tabs, value=two).classes('w-full'):
+    with ui.tab_panel(one):
+        ui.label('First tab')
+    with ui.tab_panel(two):
+        ui.label('Second tab')
 
-    # Match each tab to its content
-    with ui.tab_panels(tabs, value=tab1):
-        with ui.tab_panel(tab1):
-            home_tab()
-
-        with ui.tab_panel(tab2):
-            settings_tab()
-
-    ui.run()
-
-if __name__ in {"__main__", "__mp_main__"}:
-    main()
+ui.run()
