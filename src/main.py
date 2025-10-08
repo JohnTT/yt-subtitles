170ac7e39d6eee9117ed20913b0dbdd75aee221a
John Chen <jmc_tt@live.com>
2025-10-08 11:39:46 -0400

    Rename tabs

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from nicegui import ui
 
 with ui.tabs().classes('w-full') as tabs:
-    one = ui.tab('One')
-    two = ui.tab('Two')
-with ui.tab_panels(tabs, value=two).classes('w-full'):
-    with ui.tab_panel(one):
-        ui.label('First tab')
-    with ui.tab_panel(two):
-        ui.label('Second tab')
+    upload = ui.tab('Upload')
+    youtube = ui.tab('YouTube')
+
+with ui.tab_panels(tabs, value=youtube).classes('w-full'):
+    with ui.tab_panel(upload):
+        ui.label('Upload tab')
+    with ui.tab_panel(youtube):
+        ui.label('YouTube tab')
 
 ui.run()
