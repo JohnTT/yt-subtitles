f2343491ccfbd83608fc74b81e7dfad449e9a9fc
John Chen <jmc_tt@live.com>
2025-10-08 12:13:04 -0400

    Load whisper model on startup

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+from faster_whisper import WhisperModel
 from nicegui import ui
 from tabs import upload, youtube  # import the tab modules
 
+# Load faster-whipser translation model
+model = WhisperModel(model_size_or_path="small", device="cuda", compute_type="float16")
+
+# Start Web UI
 with ui.tabs().classes('w-full') as tabs:
     upload_tab = ui.tab('Upload')
     youtube_tab = ui.tab('YouTube')
